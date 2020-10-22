561ab506b4ba07c8f4deb3ba2196eaf9d7cbb0c4
Jingwei Too <30698779+JingweiToo@users.noreply.github.com>
2020-10-22 17:21:50 +0800

    Add files via upload
    Improve code for the fitness function

--- Main.m
+++ Main.m
@@ -14,10 +14,8 @@
 % T:     Maximum number of iterations
 % c1:    Cognitive factor
 % c2:    Social factor
-% Vmax:  Maximum velocity
 % Wmax:  Maximum bound on inertia weight
 % Wmin:  Minimum bound on inertia weight
-% *Note: k-value of KNN & hold-out setting can be modified in jFitnessFunction.m
 %---Output-----------------------------------------------------------------
 % sFeat: Selected features (instances x features)
 % Sf:    Selected feature index
@@ -25,14 +23,19 @@
 % curve: Convergence curve
 %--------------------------------------------------------------------------
 
+
 %% Binary Particle Swarm Optimization 
 clc, clear, close 
 % Benchmark data set 
 load ionosphere.mat; 
+% Set 20% data as validation set
+ho=0.2; 
+% Hold-out method
+HO=cvpartition(label,'HoldOut',ho,'Stratify',false);
 % Parameter setting
-N=10; T=100; c1=2; c2=2; Vmax=6; Wmax=0.9; Wmin=0.4; 
+N=10; T=100; c1=2; c2=2; Wmax=0.9; Wmin=0.4; 
 % Binary Particle Swarm Optimization 
-[sFeat,Sf,Nf,curve]=jBPSO(feat,label,N,T,c1,c2,Wmax,Wmin,Vmax); 
+[sFeat,Sf,Nf,curve]=jBPSO(feat,label,N,T,c1,c2,Wmax,Wmin,HO); 
 % Plot convergence curve
 figure(); plot(1:T,curve); xlabel('Number of Iterations');
 ylabel('Fitness Value'); title('BPSO'); grid on;
--- jBPSO.m
+++ jBPSO.m
@@ -1,29 +1,4 @@
-%-------------------------------------------------------------------------%
-%  Binary Particle Swarm Optimization (BPSO) source codes demo version    %
-%                                                                         %
-%  Programmer: Jingwei Too                                                %
-%                                                                         %
-%  E-Mail: jamesjames868@gmail.com                                        %
-%-------------------------------------------------------------------------%                                                                         %
-
-function [sFeat,Sf,Nf,curve]=jBPSO(feat,label,N,T,c1,c2,Wmax,Wmin,Vmax)
-%---Inputs-----------------------------------------------------------------
-% feat:  features
-% label: labelling
-% N:     Number of particles
-% T:     Maximum number of iterations
-% c1:    Cognitive factor
-% c2:    Social factor
-% Vmax:  Maximum velocity
-% Wmax:  Maximum bound on inertia weight
-% Wmin:  Minimum bound on inertia weight
-%---Outputs----------------------------------------------------------------
-% sFeat: Selected features
-% Sf:    Selected feature index
-% Nf:    Number of selected features
-% curve: Convergence curve
-%--------------------------------------------------------------------------
-
+function [sFeat,Sf,Nf,curve]=jBPSO(feat,label,N,T,c1,c2,Wmax,Wmin,HO)
 
 fun=@jFitnessFunction; 
 D=size(feat,2); X=zeros(N,D); V=zeros(N,D); 
@@ -36,16 +11,12 @@ for i=1:N
 end
 fit=zeros(1,N); fitG=inf;
 for i=1:N
-  fit(i)=fun(feat,label,X(i,:));
+  fit(i)=fun(feat,label,X(i,:),HO);
   if fit(i) < fitG
     Xgb=X(i,:); fitG=fit(i);
   end
 end
-Xpb=X; fitP=fit; 
-% Pre
-curve=inf; t=1; 
-figure(1); clf; axis([1 100 0 0.2]); xlabel('Number of Iterations');
-ylabel('Fitness Value'); title('Convergence Curve'); grid on;
+Xpb=X; fitP=fit; curve=inf; t=1; Vmax=6;
 %---Iterations start-------------------------------------------------------
 while t <= T
   w=Wmax-(Wmax-Wmin)*(t/T);
@@ -61,7 +32,7 @@ while t <= T
         X(i,d)=0;
       end
     end
-    fit(i)=fun(feat,label,X(i,:));
+    fit(i)=fun(feat,label,X(i,:),HO);
     if fit(i) < fitP(i)
       Xpb(i,:)=X(i,:); fitP(i)=fit(i);
     end
@@ -70,8 +41,7 @@ while t <= T
     end
   end
   curve(t)=fitG; 
-  pause(1e-20); hold on;
-  CG=plot(t,fitG,'Color','r','Marker','.'); set(CG,'MarkerSize',5);
+  fprintf('\nIteration %d Best (BSPO)= %f',t,curve(t))
   t=t+1;
 end
 Pos=1:D; Sf=Pos(Xgb==1); Nf=length(Sf); sFeat=feat(:,Sf); 
