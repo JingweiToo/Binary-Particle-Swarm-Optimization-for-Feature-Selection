957c6a0bd86d005ea7eb274734f89c8dd5ebb3f0
Jingwei Too <30698779+JingweiToo@users.noreply.github.com>
2020-12-19 23:13:23 +0800

    Add files via upload

--- Main.m
+++ Main.m
@@ -1,46 +1,47 @@
-%-------------------------------------------------------------------------%
-%  Binary Particle Swarm Optimization (BPSO) source codes demo version    %
-%                                                                         %
-%  Programmer: Jingwei Too                                                %
-%                                                                         %
-%  E-Mail: jamesjames868@gmail.com                                        %
-%-------------------------------------------------------------------------%
+%-------------------------------------------------------------------%
+%  Binary Particle Swarm Optimization (BPSO) demo version           %
+%-------------------------------------------------------------------%
 
 
-%---Input------------------------------------------------------------------
-% feat:  feature vector (instances x features)
-% label: labelling
-% N:     Number of particles
-% T:     Maximum number of iterations
-% c1:    Cognitive factor
-% c2:    Social factor
-% Wmax:  Maximum bound on inertia weight
-% Wmin:  Minimum bound on inertia weight
-%---Output-----------------------------------------------------------------
-% sFeat: Selected features (instances x features)
-% Sf:    Selected feature index
-% Nf:    Number of selected features
-% curve: Convergence curve
-%--------------------------------------------------------------------------
+%---Input------------------------------------------------------------
+% feat     : feature vector (instances x features)
+% label    : label vector (instances x 1)
+% N        : Number of particles
+% max_Iter : Maximum number of iterations
+% c1       : Cognitive factor
+% c2       : Social factor
+
+%---Output-----------------------------------------------------------
+% sFeat    : Selected features 
+% Sf       : Selected feature index
+% Nf       : Number of selected features
+% curve    : Convergence curve
+%--------------------------------------------------------------------
 
 
 %% Binary Particle Swarm Optimization 
 clc, clear, close 
 % Benchmark data set 
-load ionosphere.mat; 
+load ionosphere.mat;
+
 % Set 20% data as validation set
-ho=0.2; 
+ho = 0.2; 
 % Hold-out method
-HO=cvpartition(label,'HoldOut',ho,'Stratify',false);
+HO = cvpartition(label,'HoldOut',ho,'Stratify',false);
+
 % Parameter setting
-N=10; T=100; c1=2; c2=2; Wmax=0.9; Wmin=0.4; 
+N        = 10; 
+max_Iter = 100;
+c1       = 2; 
+c2       = 2; 
 % Binary Particle Swarm Optimization 
-[sFeat,Sf,Nf,curve]=jBPSO(feat,label,N,T,c1,c2,Wmax,Wmin,HO); 
+[sFeat,Sf,Nf,curve] = jBPSO(feat,label,N,max_Iter,c1,c2,HO);
+
 % Plot convergence curve
-figure(); plot(1:T,curve); xlabel('Number of Iterations');
-ylabel('Fitness Value'); title('BPSO'); grid on;
-
-
+plot(1:max_Iter,curve); 
+xlabel('Number of Iterations');
+ylabel('Fitness Value');
+title('BPSO'); grid on;
 
 
 
--- jBPSO.m
+++ jBPSO.m
@@ -1,50 +1,69 @@
-function [sFeat,Sf,Nf,curve]=jBPSO(feat,label,N,T,c1,c2,Wmax,Wmin,HO)
+function [sFeat,Sf,Nf,curve] = jBPSO(feat,label,N,max_Iter,c1,c2,HO)
+Wmax = 0.9;
+Wmin = 0.4; 
+Vmax = 6;
 
-fun=@jFitnessFunction; 
-D=size(feat,2); X=zeros(N,D); V=zeros(N,D); 
-for i=1:N
-  for d=1:D
+fun = @jFitnessFunction; 
+dim = size(feat,2); 
+X   = zeros(N,dim);
+V   = zeros(N,dim); 
+for i = 1:N
+  for d = 1:dim
     if rand() > 0.5
-      X(i,d)=1;
+      X(i,d) = 1;
     end
   end
 end
-fit=zeros(1,N); fitG=inf;
-for i=1:N
-  fit(i)=fun(feat,label,X(i,:),HO);
+fit  = zeros(1,N); 
+fitG = inf;
+for i = 1:N
+  fit(i) = fun(feat,label,X(i,:),HO);
   if fit(i) < fitG
-    Xgb=X(i,:); fitG=fit(i);
+    Xgb  = X(i,:);
+    fitG = fit(i);
   end
 end
-Xpb=X; fitP=fit; curve=inf; t=1; Vmax=6;
-%---Iterations start-------------------------------------------------------
-while t <= T
-  w=Wmax-(Wmax-Wmin)*(t/T);
-  for i=1:N
-    for d=1:D
-      r1=rand(); r2=rand();
-      VB=V(i,d)*w+c1*r1*(Xpb(i,d)-X(i,d))+c2*r2*(Xgb(d)-X(i,d)); 
-      VB(VB > Vmax)=Vmax; VB(VB < -Vmax)=-Vmax; V(i,d)=VB; 
-      TF=1/(1+exp(-V(i,d)));
+Xpb  = X; 
+fitP = fit; 
+
+curve = inf; 
+t = 1; 
+%---Iterations start-------------------------------------------------
+while t <= max_Iter
+  w = Wmax - (Wmax - Wmin) * (t / max_Iter);
+  for i = 1:N
+    for d = 1:dim
+      r1    = rand();
+      r2    = rand();
+      VB    = V(i,d) * w + c1 * r1 * (Xpb(i,d) - X(i,d)) + ...
+              c2 * r2 * (Xgb(d) - X(i,d)); 
+      VB(VB > Vmax) = Vmax;  VB(VB < -Vmax) = -Vmax; 
+      V(i,d) = VB; 
+      TF     = 1 / (1 + exp(-V(i,d)));
       if TF > rand()
-        X(i,d)=1;
+        X(i,d) = 1;
       else
-        X(i,d)=0;
+        X(i,d) = 0;
       end
     end
-    fit(i)=fun(feat,label,X(i,:),HO);
+    fit(i) = fun(feat,label,X(i,:),HO);
     if fit(i) < fitP(i)
-      Xpb(i,:)=X(i,:); fitP(i)=fit(i);
+      Xpb(i,:) = X(i,:);
+      fitP(i)  = fit(i);
     end
     if fitP(i) < fitG
-      Xgb=Xpb(i,:); fitG=fitP(i);
+      Xgb  = Xpb(i,:);
+      fitG = fitP(i);
     end
   end
-  curve(t)=fitG; 
+  curve(t) = fitG; 
   fprintf('\nIteration %d Best (BSPO)= %f',t,curve(t))
-  t=t+1;
+  t = t + 1;
 end
-Pos=1:D; Sf=Pos(Xgb==1); Nf=length(Sf); sFeat=feat(:,Sf); 
+Pos   = 1:dim;
+Sf    = Pos(Xgb == 1);
+Nf    = length(Sf);
+sFeat = feat(:,Sf); 
 end
 
 
